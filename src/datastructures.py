bd3c6f6045ac89882584a146683564cce7d09906
NahuelPerrone85 <nahuelperrone85@gmail.com>
2025-07-23 15:55:19 +0000

    falta la mitad

--- src/datastructures.py
+++ src/datastructures.py
@@ -16,7 +16,23 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane ",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
+            
+
         ]
 
     # This method generates a unique incremental ID
@@ -28,6 +44,13 @@ class FamilyStructure:
     def add_member(self, member):
         ## You have to implement this method
         ## Append the member to the list of _members
+        new_member={
+            "first_name":member["first_name"],
+                "id": self._generate_id(),
+                "age":member["age"],
+                "last_name": self.last_name,
+                "lucky_numbers":member["lucky_numbers"]}
+        self._members.append(new_member)
         pass
 
     def delete_member(self, id):
